511872aa092d3d82551648099cfa3010bb936d7f
ibnurusdianto <ibnu.rusdianto55@gmail.com>
2023-11-23 12:20:29 +0700

    menambahkan javascript untuk regis dan membuat folder backup

--- register.php
+++ register.php
@@ -53,15 +53,19 @@
                     <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password">
                     <label for="password" class="form-label">Enter your password</label>
                 </div>
+                <div class="form-check mb-3">
+                    <input class="form-check-input" type="checkbox" id="show-password">
+                    <label class="form-check-label" for="show-password">Show password</label>
+                </div>
                 <div class="mb-3">
                     <div class="form-check mb-2">
                         <input class="form-check-input" type="checkbox" value="" id="agree-term">
                         <label class="form-check-label" for="agree-term">
-                            Do you accept <a href="#">the terms of service</a>?
+                            Do you accept <a href="#" data-bs-toggle="modal" data-bs-target="#termsModal">the terms of service</a>?
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="newsletter" checked>
+                        <input class="form-check-input" type="checkbox" value="" id="newsletter">
                         <label class="form-check-label" for="newsletter">
                             Do you want to receive our newsletters?
                         </label>
@@ -83,6 +87,29 @@
     </div>
 </div>
 <!--End Form-->
+
+
+<!--Modal term of services-->
+<div class="modal fade" id="termsModal" tabindex="-1" role="dialog" aria-labelledby="termsModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header justify-content-center">
+                <h5 class="modal-title w-100 text-center" id="termsModalLabel">The Terms of Service</h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body d-flex justify-content-between">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt, mauris et imperdiet ultricies, sapien turpis elementum ipsum, in sagittis velit justo et lorem.</p>
+                <img src="img/4.jpg" alt="Thumbnail" class="img-thumbnail">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--End Modals-->
 
 
 <!-- info section -->
@@ -112,6 +139,7 @@
 </footer>
 <!-- end footer section -->
 
+<script src="javascript/register.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
