d355fc23db4ba3ee779a99efce69bd330a6d91ad
ibnurusdianto <ibnu.rusdianto55@gmail.com>
2023-11-29 09:02:26 +0700

    keamanan brute force attack dari login

--- function-login-diluar-admin/login.php
+++ function-login-diluar-admin/login.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+
+$max_attempts = 3;
+$delay = 30;
+
 if (isset($_SESSION['username'])) {
     header('Location: ../index.php');
     exit;
@@ -35,6 +39,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
+        if (isset($_SESSION['login_attempts']) && $_SESSION['login_attempts'] >= $max_attempts) {
+            if (time() - $_SESSION['last_attempt_time'] < $delay) {
+                echo "<script>alert('Anda sudah mencoba login sebanyak 3x dan tunggu 30 detik untuk melakukan login kembali!');</script>";
+                exit;
+            } else {
+                $_SESSION['login_attempts'] = 0;
+            }
+        }
+
         $loginResult = login($username, $password);
 
         if ($loginResult == "admin") {
@@ -42,6 +55,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         } elseif ($loginResult == "user"){
             header('Location: ../index.php');
         } else {
+            if (!isset($_SESSION['login_attempts'])) {
+                $_SESSION['login_attempts'] = 1;
+            } else {
+                $_SESSION['login_attempts']++;
+            }
+            $_SESSION['last_attempt_time'] = time();
             echo 'Login gagal';
         }
     }
