2b8223c6b38e87a5df8296103421e73ad10017f1
ibnurusdianto <ibnu.rusdianto55@gmail.com>
2023-12-03 15:12:33 +0700

    deleted all backup

--- backup/backup12/function-login-diluar-admin/user-logout-sesi.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['username'])) {
-    unset($_SESSION['username']);
-    session_destroy();
-}
-?>
--- backup/backupv9 - admin/admin/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$db_name = "travel";
-$conn = new mysqli($hostname, $username, $password, $db_name);
