ec05435b9a1c811f3293091961f6fcccbc60bb39
ibnurusdianto <ibnu.rusdianto55@gmail.com>
2023-12-07 16:01:31 +0700

    regex for new password

--- profile-user/php-function/edit-user.php
+++ profile-user/php-function/edit-user.php
@@ -21,6 +21,8 @@ if (isset($_POST['simpan'])) {
     if ($new_password != "" && $confirm_password != "" && $new_image["name"] == "") {
         if ($new_password != $confirm_password) {
             echo "<script>alert('Password tidak sama');</script>";
+        } elseif (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/", $new_password)) {
+            echo "<script>alert('Password harus terdiri dari setidaknya satu huruf kecil, satu huruf besar, satu angka, satu karakter khusus, dan panjang minimal 8 karakter.');</script>";
         } else {
             $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
             $query = mysqli_query($conn, "UPDATE user SET password='$hashed_password' WHERE id='$id'");
@@ -31,9 +33,12 @@ if (isset($_POST['simpan'])) {
                 echo "<script>alert('Data gagal diupdate');</script>";
             }
         }
-    } elseif ($new_password != "" && $confirm_password != "" && $new_image["name"] != "") {
+    }
+    if ($new_password != "" && $confirm_password != "" && $new_image["name"] != "") {
         if ($new_password != $confirm_password) {
             echo "<script>alert('Password tidak sama');</script>";
+        } elseif (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/", $new_password)) {
+            echo "<script>alert('Password harus terdiri dari setidaknya satu huruf kecil, satu huruf besar, satu angka, satu karakter khusus, dan panjang minimal 8 karakter.');</script>";
         } else {
             if ($file_size > 40000000) {
                 echo "<script>alert('Gambar terlalu besar!');</script>";
@@ -65,8 +70,9 @@ if (isset($_POST['simpan'])) {
                 }
             }
         }
-    } elseif ($new_password == "" && $confirm_password == "" && $new_image["name"] != "") {
-        if ($file_size > 40000000) {
+    }
+    elseif ($new_password == "" && $confirm_password == "" && $new_image["name"] != "") {
+        if ($file_size > 4000000) {
             echo "<script>alert('Gambar terlalu besar!');</script>";
             echo "<script>window.location='../profile.php';</script>";
         } elseif (!in_array($file_type, $allowed_types)) {
@@ -78,16 +84,18 @@ if (isset($_POST['simpan'])) {
         } elseif ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
             echo "<script>alert('Maaf, dibilang hanya bisa upload JPG, JPEG, PNG');</script>";
             echo "<script>window.location='../profile.php';</script>";
-        } elseif ($file_size > 40000000) {
+        } elseif ($file_size > 4000000) {
             echo "<script>alert('Maaf, Dibilang Gambar terlalu besar!');</script>";
             echo "<script>window.location='../profile.php';</script>";
+        } elseif (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?])[A-Za-z\d@$!%*?]{8,}$/", $password)) {
+            echo "Password harus terdiri dari setidaknya satu huruf kecil, satu huruf besar, satu angka, satu karakter khusus, dan panjang minimal 8 karakter.";
         } elseif (!move_uploaded_file($new_image["tmp_name"], $target_file)) {
             echo "<script>alert('Maaf, ada kesalahan dalam upload file, mohon dicek kembali');</script>";
             echo "<script>window.location='../profile.php';</script>";
         } else {
             $query = mysqli_query($conn, "UPDATE user SET img='$img' WHERE id='$id'");
             if ($query) {
-                move_uploaded_file($_FILES['img']['tmp_name'], "../../img/uploads/".$_FILES['img']['name']);
+                move_uploaded_file($_FILES['img']['tmp_name'], $target_file);
                 echo "<script>alert('Data berhasil diupdate, tanpa update password');</script>";
                 echo "<script>window.location='../profile.php';</script>";
             } else {
